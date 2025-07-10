be6408151993d111d0bea3459faf51c246509b8a
FuFacu <fuyuestudios@gmail.com>
2025-07-10 18:17:32 +0000

    Delete Parciales/parciales_hechos/parcial2/parcial2/src/main/java/org/example directory

--- Parciales/parciales_hechos/parcial2/parcial2/src/main/java/org/example/EstadoFinalizado.java
+++
@@ -1,8 +0,0 @@
-package org.example;
-
-public class EstadoFinalizado implements IEstado {
-
-	public void completar(ITarea tarea) {}
-	public void empezar(ITarea tarea) {}
-	
-}
--- Parciales/parciales_hechos/parcial2/parcial2/src/main/java/org/example/IEstado.java
+++
@@ -1,8 +0,0 @@
-package org.example;
-
-public interface IEstado {
-	
-	void completar(ITarea tarea);
-	void empezar(ITarea tarea);
-
-}
